29a42a81980d7fb788ab77111d1a0c2377166817
weiawe <553054879@qq.com>
2019-07-30 21:40:28 +0800

    update V0.1 gitignore

---
+++ src/com/weiawe/test/Alibaba.java
@@ -0,0 +1,23 @@
+package com.weiawe.test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Alibaba {
+
+	public static void main(String[] args) {
+		String[] str = new String[]{"abc","123"};
+		List list = Arrays.asList( str);
+		
+		String a = null;
+		switch (a) {
+		case "null":
+			System.out.println("null");
+			break;
+
+		default:
+			System.out.println("default");
+			break;
+		}
+	}
+}
